3d9cc3e4c0dbbb04c6c8d63532ac0721a463eb3f
ShairYousuf <syjahin@mun.ca>
2023-05-30 08:03:50 -0230

    plot output

--- main.py
+++ main.py
@@ -15,4 +15,24 @@ step = 0.02
 X = torch.arange(start, end, step).unsqueeze(dim=1)
 y = weight * X + bias
 
-print(X[:10],y[:10])
+#Create a train/test split
+train_split = int(0.8*len(X))
+X_train = X[:train_split]
+Y_train = y[:train_split]
+X_test = X[train_split:]
+y_test = y[train_split:]
+
+def plot_predictions(train_data=X_train, 
+                     train_labels=Y_train, 
+                     test_data=X_test, 
+                     test_labels=y_test, 
+                     predictions=None):
+    plt.figure(figsize=(10, 7))
+    plt.scatter(train_data, train_labels, c='b', label='Training data')
+    plt.scatter(test_data, test_labels, c='g', label='Testing data')
+    if predictions is not None:
+        plt.scatter(train_data, predictions, c='r', label='Predictions')
+    plt.legend(prop = {'size': 14})
+    plt.show()
+
+plot_predictions()
